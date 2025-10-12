0fc3f938aa0382d95b2428d0f22778f26cf57177
Ahmadou Gassam <melodygazzam1@gmail.com>
2025-10-12 15:16:41 -0500

    Revert "deleted vpc block"
    This reverts commit bfd0260b085418cda92ffe8558ec0576265271e4.

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,13 +1,13 @@
 
 #CREATING VPC ------------------------------------------------------------------------------------------ 
-#resource "aws_vpc" "main_vpc" {
-  #cidr_block       = var.vpc_cidr_block
- # instance_tenancy = "default" 
+resource "aws_vpc" "main_vpc" {
+  cidr_block       = var.vpc_cidr_block
+  instance_tenancy = "default" 
 
-#tags = merge(var.tags, {
-  #  Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-main-vpc"
- # })
-#}
+tags = merge(var.tags, {
+    Name = "${var.tags["project"]}-${var.tags["application"]}-${var.tags["environment"]}-main-vpc"
+  })
+}
 
 
 #CREATING INTERNET GATEWAY ------------------------------------------------------------------------------------
